2a9fb1db90c9c78f7abce332fa0a226538175883
Lebohang Sopazi <72252063+DaLebza10111@users.noreply.github.com>
2023-07-18 17:22:52 +0200

    Add project files.

---
+++ CodingChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CodingChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Enter the number of songs for your playlist");
+            int totalnumberofsongs = int.Parse(Console.ReadLine());
+
+            List<int> songs = new List<int>();
+
+            for (int i = 0; i < totalnumberofsongs; i++)
+            {
+                songs.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine("\nPlease add another song");
+            }
+
+        }
+    }
+}
